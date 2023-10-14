87f0d5d5e3dca64f18e06b6d4e2777b8df970fe2
shinmin <shinmin9812@naver.com>
2023-10-14 17:42:08 +0900

    feat: 채용공고 등록 API 구현

--- src/main/java/com/example/wanted/controller/RecruitController.java
+++ src/main/java/com/example/wanted/controller/RecruitController.java
@@ -1,10 +1,24 @@
 package com.example.wanted.controller;
 
+import com.example.wanted.dto.RecruitRequestDto;
+import com.example.wanted.service.RecruitService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/recruit")
+@RequiredArgsConstructor
 public class RecruitController {
+    private final RecruitService recruitService;
+
+    @PostMapping("/create")
+    public void create(@RequestBody RecruitRequestDto recruitRequestDto) {
+        recruitService.create(recruitRequestDto);
+    }
 
 }
--- src/main/java/com/example/wanted/service/RecruitService.java
+++ src/main/java/com/example/wanted/service/RecruitService.java
@@ -1,8 +1,22 @@
 package com.example.wanted.service;
 
+import com.example.wanted.domain.Recruit;
+import com.example.wanted.dto.RecruitRequestDto;
+import com.example.wanted.repository.RecruitRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class RecruitService {
+    private final RecruitRepository recruitRepository;
+
+    //채용공고 생성
+    public void create(RecruitRequestDto recruitRequestDto) {
+        Recruit recruit = RecruitRequestDto.toRecruit(recruitRequestDto);
+        recruitRepository.save(recruit);
+    }
 
 }
